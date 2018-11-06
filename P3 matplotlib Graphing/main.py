12fd5e0c4182ec65d0934732a631c68b8913f030
Rodrigo Becerril Ferreyra <rodrigo.becerrilferreyra@student.csulb.edu>
2018-11-06 09:31:13 -0800

    Added simple menu to main()

--- P3 matplotlib Graphing/main.py
+++ P3 matplotlib Graphing/main.py
@@ -9,3 +9,30 @@
 This program is intended to graph the line defined either by two points
 or by a slope and a y-intercept. The required parameters will be inputed by the user.
 '''
+
+import matplotlib.pyplot as plt
+
+def twopoints():
+	print("\ntwo points")
+
+def slopeint():
+	print("\nslopeint")
+
+def main():
+	print("Plot a graph using pyplot!")
+	print("\nEnter a 1 or a 2\n1: Plot a line using two points\n2: Plot a line using a slope and a y-intercept.")
+	
+	try:
+		ui = int(input("\n"))
+
+		if ui == 1:
+			twopoints()
+		elif ui == 2:
+			slopeint()
+		else:
+			raise(ValueError)
+
+	except ValueError:
+		print("Please enter a 1 or a 2!")
+
+main()	
