29285a0466eb36540efefacfa46be30e037fec3c
Rodrigo Becerril Ferreyra <rodrigo.becerrilferreyra@student.csulb.edu>
2018-11-06 09:14:27 -0800

    Initialized Project 3

---
+++ P3 matplotlib Graphing/main.py
@@ -0,0 +1,11 @@
+# CECS 100 Project 3
+# "Graphing a line using matplotlib and pyplot"
+# Start date: 06 November 2018
+# End date: 08 November 2018
+# Name: Rodrigo Becerril Ferreyra
+#ID: 017584071
+
+'''
+This program is intended to graph the line defined either by two points
+or by a slope and a y-intercept. The required parameters will be inputed by the user.
+'''
