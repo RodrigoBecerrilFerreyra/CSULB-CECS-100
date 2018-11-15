d6e836d7ae3980508a0cd2dd79b00eb37b6f94ae
Rodrigo Becerril Ferreyra <rodrigo.becerrilferreyra@student.csulb.edu>
2018-11-15 09:20:56 -0800

    Started and initialized P4

---
+++ P4/main.py
@@ -0,0 +1,11 @@
+# CECS 100 Project 4
+# Reference: Text pages 293 - 295
+# 11 November 2018
+# Rodrigo Becerril Ferreyra
+# CSULB ID 017584071
+
+# We will write data to a text file.
+
+def main():
+    pass
+main()
