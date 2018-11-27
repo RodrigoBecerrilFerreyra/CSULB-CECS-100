ec63e095ab638a2ca249b1cd8a331a52b26abc2b
Rodrigo Becerril Ferreyra <rodrigo.becerrilferreyra@student.csulb.edu>
2018-11-27 09:38:04 -0800

    Added description to P5 main.py

---
+++ P5/main.py
@@ -0,0 +1,14 @@
+# CECS 100 Fall 2018
+# Project 5
+# "Bubble Sort"
+# Start date: 2018-11-27
+# End date: 
+# Name: Rodrigo Becerril Ferreyra
+# ID Number: 017584071
+# Description:
+# 	Manual bubble sort algorithm
+
+def main():
+	pass
+	
+main()
