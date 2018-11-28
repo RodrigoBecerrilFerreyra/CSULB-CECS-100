5e02937313ad33633eec5e5f46e1cfe3f2b59aee
Rodrigo Becerril Ferreyra <rodrigo.becerrilferreyra@student.csulb.edu>
2018-11-27 18:58:14 -0800

    Finished up bubble()

--- P5 Bubble Sort/main.py
+++ P5 Bubble Sort/main.py
@@ -8,22 +8,28 @@
 # Description:
 # 	Manual bubble sort algorithm
 
-def bubble(oldlist, end):
-	"""Usage: newlist = bubble(oldlist, len(oldlist))
+def bubble(oldlist, end=-1):
+	"""Usage: newlist = bubble(oldlist)
 
 	Returns a list sorted from least to greatest.
 	"""
+	newlist = oldlist
+	if end==-1:
+		end = len(newlist)
+	
 	if end <= 1:
-		return oldlist
+		return newlist
 	else:
-		for i in range(end):
-			if oldlist[i] < oldlist[i+1]:
-				oldlist[i] = oldlist[i] + oldlist[i+1]
-				oldlist[i+1] = oldlist[i] - oldlist[i+1]
-				oldlist[i] = oldlist[i] - oldlist[i+1]
-		bubble(oldlist, end-1)
+		for i in range(end-1):
+			if newlist[i] > newlist[i+1]:
+				newlist[i] = newlist[i] + newlist[i+1]
+				newlist[i+1] = newlist[i] - newlist[i+1]
+				newlist[i] = newlist[i] - newlist[i+1]
+
+		return bubble(newlist, end-1)
 
 def main():
-	pass
+	list1 = [5, 2, 1, -6, 54, 4, 1, 1]
+	print(bubble(list1))
 	
 main()
