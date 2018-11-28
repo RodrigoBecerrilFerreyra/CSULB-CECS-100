65a394b7d71323beec9fc7ff8f142699e3a9bea4
Rodrigo Becerril Ferreyra <rodrigo.becerrilferreyra@student.csulb.edu>
2018-11-27 19:03:50 -0800

    Added comments to P5

--- P5 Bubble Sort/main.py
+++ P5 Bubble Sort/main.py
@@ -14,19 +14,23 @@ def bubble(oldlist, end=-1):
 	Returns a list sorted from least to greatest.
 	"""
 
+	# Make end equal to the length of the list if function is called outside of function
 	if end==-1:
 		end = len(oldlist)
 	
+	# Base case: the function is finished, and shuld be returned
 	if end <= 1:
 		return oldlist
+	# Recursive case: calculate list and try again	
 	else:
 		for i in range(end-1):
 			if oldlist[i] > oldlist[i+1]:
+				# swap oldlist[i] with oldlist[i+1]
 				oldlist[i] = oldlist[i] + oldlist[i+1]
 				oldlist[i+1] = oldlist[i] - oldlist[i+1]
 				oldlist[i] = oldlist[i] - oldlist[i+1]
 
-		return bubble(oldlist, end-1)
+		return bubble(oldlist, end-1) #recursive (calls itself)
 
 def main():
 	print(bubble([5, 2, 6, 1]))
