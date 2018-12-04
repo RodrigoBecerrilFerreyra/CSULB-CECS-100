bddc55982b452f46e6c17733f703820b639f16f6
Rodrigo Becerril Ferreyra <rodrigo.becerrilferreyra@student.csulb.edu>
2018-12-04 09:50:55 -0800

    Fixed bug where if array was already in order the swap index was replaced with 0

--- P6 Selection Sort/main.py
+++ P6 Selection Sort/main.py
@@ -3,20 +3,19 @@
 # CECS 100
 # Name: Rodrigo Becerril Ferreyra
 # ID: 017584071
-def selection(oldlist, start=-1):
-    if start == -1:
-        start = 0
+def selection(oldlist, start=0):
 
     if start == len(oldlist) - 1:
         return oldlist
     else:
         swap = oldlist.index(min(oldlist[start:]))
-        oldlist[start] = oldlist[start] + oldlist[swap]
-        oldlist[swap] = oldlist[start] - oldlist[swap]
-        oldlist[start] = oldlist[start] - oldlist[swap]
+        if oldlist[swap] < oldlist[start]:
+            oldlist[start] = oldlist[start] + oldlist[swap]
+            oldlist[swap] = oldlist[start] - oldlist[swap]
+            oldlist[start] = oldlist[start] - oldlist[swap]
         return selection(oldlist, start+1)
 
 def main():
-    print(selection([15, 10, 13]))
+    print(selection([10, 9, 8, 7, 0, 5, 4, 3, 2, 1]))
 
 main()
