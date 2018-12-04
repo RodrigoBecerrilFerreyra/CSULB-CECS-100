0e00a82f65832dd3c092bba53c56596655e8fe40
Rodrigo Becerril Ferreyra <rodrigo.becerrilferreyra@student.csulb.edu>
2018-12-04 09:39:04 -0800

    Added backend for selection sort (P6)

---
+++ P6 Selection Sort/main.py
@@ -0,0 +1,17 @@
+def selection(oldlist, start=-1):
+    if start == -1:
+        start = 0
+
+    if start == len(oldlist) - 1:
+        return oldlist
+    else:
+        swap = oldlist.index(min(oldlist[start:]))
+        oldlist[start] = oldlist[start] + oldlist[swap]
+        oldlist[swap] = oldlist[start] - oldlist[swap]
+        oldlist[start] = oldlist[start] - oldlist[swap]
+        return selection(oldlist, start+1)
+
+def main():
+    print(selection([15, 10, 13]))
+
+main()
