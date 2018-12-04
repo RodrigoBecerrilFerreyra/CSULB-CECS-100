4c964b97da452e222e2e98af36d69a421956cf8d
Rodrigo Becerril Ferreyra <rodrigo.becerrilferreyra@student.csulb.edu>
2018-12-04 09:58:10 -0800

    Added comments P6

--- P6 Selection Sort/main.py
+++ P6 Selection Sort/main.py
@@ -5,15 +5,20 @@
 # ID: 017584071
 def selection(oldlist, start=0):
 
+    # Base case: list is already sorted
     if (start <= len(oldlist) - 1) or (len(oldlist) == 0):
         return oldlist
+    # Recursive case: Continue with swap
     else:
+        # Find the index of the smallest integer
         swap = oldlist.index(min(oldlist[start:]))
+        # If the smallest integer is not in the start index
         if oldlist[swap] < oldlist[start]:
+            # swap
             oldlist[start] = oldlist[start] + oldlist[swap]
             oldlist[swap] = oldlist[start] - oldlist[swap]
             oldlist[start] = oldlist[start] - oldlist[swap]
-        return selection(oldlist, start+1)
+        return selection(oldlist, start+1) #recursive function
 
 def main():
     print(selection([10, 9, 8, 7, 0, 5, 4, 3, 2, 1]))
